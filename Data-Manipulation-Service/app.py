bfdc6570ba831db4218dd6a1f480072320c1d8d7
Bence Nagy <nagybence0601@gmail.com>
2024-12-01 17:42:06 +0100

    fix(Data-Manipulation-Service): Added CORS

--- Data-Manipulation-Service/app.py
+++ Data-Manipulation-Service/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from routes import data_manipulation_bp
+from flask_cors import CORS 
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(data_manipulation_bp)
 
